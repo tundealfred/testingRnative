249c42acb81a7b35c93badcff7cce69ae5dd4005
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-05-19 16:37:24 +0100

    add about page

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>About Screen</Text>
+      <Text>Testing to see how the About us page works</Text>
+    </View>
+  );
+}
