ceb2bb4b2c981e7a7151b2799a6c39bf5a30b29c
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-05-20 15:09:25 +0100

    add more pages

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
 
 export default function AboutScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>About Screen</Text>
-      <Text>Testing to see how the About us page works</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>About Page | Next React Native Page</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="app/(tabs)/about.tsx" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
---
+++ app/(tabs)/contact.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Contact Screen</Text>
+      <Text>Testing to see how the Contact page works and looks</Text>
+    </View>
+  );
+}
